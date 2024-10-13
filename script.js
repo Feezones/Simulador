1fe88b216add94abea55b55331618f89ed9614a9
Felipe Braghittoni <felipe.braghittoni1999@gmail.com>
2024-10-13 20:52:54 -0300

    umas coisas ae

--- script.js
+++ script.js
@@ -2,33 +2,33 @@
 loadJson()
 var Combinations;
 
-function loadJson(){
+function loadJson() {
     // Usando fetch para carregar o arquivo JSON
     fetch('combinations.json')  // Especifique o caminho do arquivo JSON
-    .then(response => {
-        // Verificar se a resposta foi bem-sucedida
-        if (!response.ok) {
-            throw new Error('Erro ao carregar o arquivo JSON.');
-        }
+        .then(response => {
+            // Verificar se a resposta foi bem-sucedida
+            if (!response.ok) {
+                throw new Error('Erro ao carregar o arquivo JSON.');
+            }
 
-        return response.json(); // Converter a resposta em JSON
-    })
-    .then(data => {
-        // Manipular os dados do JSON
-        Combinations = data
-        //alert(data)
-    })
-    .catch(error => {
-        // Manipular erros
-        console.error('Erro:', error);
-    });
+            return response.json(); // Converter a resposta em JSON
+        })
+        .then(data => {
+            // Manipular os dados do JSON
+            Combinations = data
+            //alert(data)
+        })
+        .catch(error => {
+            // Manipular erros
+            console.error('Erro:', error);
+        });
 }
 
 
 
 
 
-function sumValues(){
+function sumValues() {
     var vlrMovel = document.getElementById("vlrmovel").value;
     var vlrBl = document.getElementById("vlrbl").value;
 
@@ -38,92 +38,110 @@ function sumValues(){
     showPlan()
 }
 
-function showPlan(){
+function showPlan() {
     var selectedlinhas
 
     var qtdLinhas = document.getElementById("linhas").value;
     qtdLinhas = parseInt(qtdLinhas)
 
+    var vlrTotal = document.getElementById("vfl").value;
+    vlrTotal = parseInt(vlrTotal)
+
     var BaseUR = document.getElementById("baseUR")
     var Port = document.getElementById("Port")
 
-    if(!Port.checked || !BaseUR.checked){
-        switch(qtdLinhas){
+    if (!Port.checked || !BaseUR.checked) {
+        switch (qtdLinhas) {
             case 1:
                 selectedlinhas = Object.values(Combinations.Combinations).filter(x => x.Linhas >= qtdLinhas);
-            break;
+                break;
             case 2:
                 selectedlinhas = Object.values(Combinations.Combinations).filter(x => x.Linhas.includes(qtdLinhas));
-            break;
+                break;
             case 3:
                 selectedlinhas = Object.values(Combinations.Combinations).filter(x => x.Linhas.includes(qtdLinhas));
-            break;
+                break;
             case 4:
                 selectedlinhas = Object.values(Combinations.Combinations).filter(x => x.Linhas.includes(qtdLinhas));
-            break;
+                break;
             case 5:
                 selectedlinhas = Object.values(Combinations.Combinations).filter(x => x.Linhas.includes(qtdLinhas));
-            break;
+                break;
             case 6:
                 selectedlinhas = Object.values(Combinations.Combinations).filter(x => x.Linhas.includes(qtdLinhas));
-            break;
+                break;
             default:
                 selectedlinhas = Object.values(Combinations.Combinations).filter(x => x.Linhas >= 1);
         }
-    }else{
+    } else {
         selectedlinhas = URandPort(qtdLinhas);
     }
 
+    selectedlinhas = filterByValue(selectedlinhas, vlrTotal)
 
-        document.getElementById("faixaNome1").innerText = selectedlinhas[0].InternetMovel;
-        document.getElementById("vlr_movel1").innerText = selectedlinhas[0].vlr_movel;
-        document.getElementById("ibl1").innerText = selectedlinhas[0].InternetBandaLarga;
-        document.getElementById("vlrBl1").innerText = selectedlinhas[0].vlr_bl;
-        document.getElementById("totalFaixa1").innerText = 
-            qtdLinhas > 1 ? selectedlinhas[0].vlr_total + ((qtdLinhas - 1) * 50) 
-                : selectedlinhas[0].vlr_total;
-        
-        document.getElementById("faixaNome2").innerText = selectedlinhas[1].InternetMovel;
-        document.getElementById("vlr_movel2").innerText = selectedlinhas[1].vlr_movel;
-        document.getElementById("ibl2").innerText = selectedlinhas[1].InternetBandaLarga;
-        document.getElementById("vlrBl2").innerText = selectedlinhas[1].vlr_bl;
-        document.getElementById("totalFaixa2").innerText = 
-            qtdLinhas > 1 ? selectedlinhas[1].vlr_total + ((qtdLinhas - 1) * 50) 
-                : selectedlinhas[1].vlr_total;
 
-        document.getElementById("faixaNome3").innerText = selectedlinhas[2].InternetMovel;
-        document.getElementById("vlr_movel3").innerText = selectedlinhas[2].vlr_movel;
-        document.getElementById("ibl3").innerText = selectedlinhas[2].InternetBandaLarga;
-        document.getElementById("vlrBl3").innerText = selectedlinhas[2].vlr_bl;
-        document.getElementById("totalFaixa3").innerText = 
-            qtdLinhas > 1 ? selectedlinhas[2].vlr_total + ((qtdLinhas - 1) * 50) 
-                : selectedlinhas[2].vlr_total;
+    document.getElementById("faixaNome1").innerText = selectedlinhas[0].InternetMovel;
+    document.getElementById("vlr_movel1").innerText = selectedlinhas[0].vlr_movel;
+    document.getElementById("ibl1").innerText = selectedlinhas[0].InternetBandaLarga;
+    document.getElementById("vlrBl1").innerText = selectedlinhas[0].vlr_bl;
+    document.getElementById("totalFaixa1").innerText =
+        qtdLinhas > 1 ? selectedlinhas[0].vlr_total + ((qtdLinhas - 1) * 50)
+            : selectedlinhas[0].vlr_total;
 
-        document.getElementById("faixaNome4").innerText = selectedlinhas[3].InternetMovel;
-        document.getElementById("vlr_movel4").innerText = selectedlinhas[3].vlr_movel;
-        document.getElementById("ibl4").innerText = selectedlinhas[3].InternetBandaLarga;
-        document.getElementById("vlrBl4").innerText = selectedlinhas[3].vlr_bl;
-        document.getElementById("totalFaixa4").innerText = 
-            qtdLinhas > 1 ? selectedlinhas[3].vlr_total + ((qtdLinhas - 1) * 50) 
-                : selectedlinhas[3].vlr_total;
+    document.getElementById("faixaNome2").innerText = selectedlinhas[1].InternetMovel;
+    document.getElementById("vlr_movel2").innerText = selectedlinhas[1].vlr_movel;
+    document.getElementById("ibl2").innerText = selectedlinhas[1].InternetBandaLarga;
+    document.getElementById("vlrBl2").innerText = selectedlinhas[1].vlr_bl;
+    document.getElementById("totalFaixa2").innerText =
+        qtdLinhas > 1 ? selectedlinhas[1].vlr_total + ((qtdLinhas - 1) * 50)
+            : selectedlinhas[1].vlr_total;
 
-        document.getElementById("faixaNome5").innerText = selectedlinhas[4].InternetMovel;
-        document.getElementById("vlr_movel5").innerText = selectedlinhas[4].vlr_movel;
-        document.getElementById("ibl5").innerText = selectedlinhas[4].InternetBandaLarga;
-        document.getElementById("vlrBl5").innerText = selectedlinhas[4].vlr_bl;
-        document.getElementById("totalFaixa5").innerText = 
-            qtdLinhas > 1 ? selectedlinhas[4].vlr_total + ((qtdLinhas - 1) * 50) 
-                : selectedlinhas[4].vlr_total;
+    document.getElementById("faixaNome3").innerText = selectedlinhas[2].InternetMovel;
+    document.getElementById("vlr_movel3").innerText = selectedlinhas[2].vlr_movel;
+    document.getElementById("ibl3").innerText = selectedlinhas[2].InternetBandaLarga;
+    document.getElementById("vlrBl3").innerText = selectedlinhas[2].vlr_bl;
+    document.getElementById("totalFaixa3").innerText =
+        qtdLinhas > 1 ? selectedlinhas[2].vlr_total + ((qtdLinhas - 1) * 50)
+            : selectedlinhas[2].vlr_total;
+
+    document.getElementById("faixaNome4").innerText = selectedlinhas[3].InternetMovel;
+    document.getElementById("vlr_movel4").innerText = selectedlinhas[3].vlr_movel;
+    document.getElementById("ibl4").innerText = selectedlinhas[3].InternetBandaLarga;
+    document.getElementById("vlrBl4").innerText = selectedlinhas[3].vlr_bl;
+    document.getElementById("totalFaixa4").innerText =
+        qtdLinhas > 1 ? selectedlinhas[3].vlr_total + ((qtdLinhas - 1) * 50)
+            : selectedlinhas[3].vlr_total;
+
+    document.getElementById("faixaNome5").innerText = selectedlinhas[4].InternetMovel;
+    document.getElementById("vlr_movel5").innerText = selectedlinhas[4].vlr_movel;
+    document.getElementById("ibl5").innerText = selectedlinhas[4].InternetBandaLarga;
+    document.getElementById("vlrBl5").innerText = selectedlinhas[4].vlr_bl;
+    document.getElementById("totalFaixa5").innerText =
+        qtdLinhas > 1 ? selectedlinhas[4].vlr_total + ((qtdLinhas - 1) * 50)
+            : selectedlinhas[4].vlr_total;
 }
 
-function URandPort(qtdLinhas){
+function URandPort(qtdLinhas) {
     var CombinationsWithDesc = Object.values(Combinations.Combinations)
-        .filter(x => (x.InternetMovel == "28GB") 
-            || (x.InternetMovel == "100GB") 
-                || (x.InternetMovel == "200GB") 
-                    || (x.InternetMovel == "300GB") 
-                        && (x.Linhas.includes(qtdLinhas)));
+        .filter(x => (x.InternetMovel == "28GB")
+            || (x.InternetMovel == "100GB")
+            || (x.InternetMovel == "200GB")
+            || (x.InternetMovel == "300GB")
+            && (x.Linhas.includes(qtdLinhas)));
 
     return CombinationsWithDesc
-    
+
+}
+
+function filterByValue(selectedlinhas, vlrTotal){
+    //selectedlinhas = Object.values(selectedlinhas).filter(x => x.vlr_total >= Math.floor(vlrTotal));
+    //return selectedlinhas
+
+    const listaOrdenada = selectedlinhas.map(faixa =>({
+        ...faixa,
+        diferenca:Math.abs(faixa.vlr_total - vlrTotal)
+    }))
+    .sort((a, b) => a.diferenca - b.diferenca)
+
+    return listaOrdenada.slice(0, 5);
 }
